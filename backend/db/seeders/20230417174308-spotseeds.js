52be2c4f25cd4c6b5f2f037061565e5331126bb2
Taylor <koo2true@gmail.com>
2023-04-17 10:50:31 -0700

    added spot seeds

---
+++ backend/db/seeders/20230417174308-spotseeds.js
@@ -0,0 +1,29 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+   options.tableName = 'Spots';
+   return queryInterface.bulkInsert(options,[
+    {
+      
+    }
+   ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
