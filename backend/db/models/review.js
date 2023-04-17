9c4c188149ac960b0a152adf49996f89be3999c0
Taylor <koo2true@gmail.com>
2023-04-17 12:09:04 -0700

    added review association

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -10,7 +10,15 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Review.belongsTo(models.Spot,{foreignKey:"spotId"})
+      Review.belongsTo(models.User,{foreignKey:"userId"})
+      Review.hasMany(models.reviewImage,{
+        foreignKey:'reviewId',
+        onDelete:"CASCADE",
+        hooks:true
+      })
+
+
     }
   }
   Review.init({
