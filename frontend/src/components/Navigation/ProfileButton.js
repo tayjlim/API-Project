6cee1e132a975ae02454b71a3f7675d6ebb522da
Taylor <koo2true@gmail.com>
2023-05-15 00:11:09 -0700

    finsihed i hope

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -63,8 +63,8 @@ function ProfileButton({ user }) {
       <ul className={ulClassName} ref={ulRef}>
         {user ? (
           <>
-            <li>Hello, {user.username}</li>
-            <li>{user.firstName} {user.lastName}</li>
+
+            <li>Hello, {user.firstName}, {user.lastName}!</li>
             <li>{user.email}</li>
             <li>
               <button className = 'logOutButton' onClick={logout} >Log Out</button>
